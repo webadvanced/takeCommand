fb66c90ac77edb23c334da888944bae24aaa1c8e
Web Advanced <paul.barton@webadvanced.com>
2012-07-19 13:50:59 -0700

    updated read me. Updated call structure

--- readme.md
+++ readme.md
@@ -3,19 +3,28 @@
 ```javascript
 
 var addUserCommand = commands.register( 'addUserCommand', { url: '/users/add' } );
+//or
+commands.register('updateUserCommand', { url: '/users/update '});
 
 addUserCommand.success( function() {
-    alert('success!');
+    alert( 'success!' );
 }).error( function() {
     alert('there was a problem');
 }).always(function() {
     //clear form fields
 });
 
+commands.updateUserCommand.success( function() {
+    alert( 'success!' );
+});
+
 //Manualy execute
-commands.execute.addUserCommand();
+commands.addUserCommand.execute();
 
 //Use as a callback
 $('#addUserForm').submit(commands.execute.addUserCommand);
+commands.updateUserCommand.bind('#formUpdate', 'submit', function() {
+    return $( this ).serialize(); //return the data to be passed into the Ajax call
+});
 
 ```
--- src/takeCommand.js
+++ src/takeCommand.js
@@ -3,49 +3,67 @@ var commands = ( function( $ ) {
         throw 'jQuery is required.';
     }
 
-    var commands = { execute: {} },
+    var commands = { },
         _defaultOptions = { type: 'GET',  dataType: 'JSON' },
-        _isFunction;
+        _isFunction,
+        Commad,
+        checkArg = {};
+
+    checkArg.required = function( obj, message ) {
+        if( !obj ) {
+            throw message;
+        }
+    };
+
+    Command = function( key, options ) {
+        var self = this;
+        this.key = key;
+        this.options = options;
+        this.send = this.execute = function( data ) {
+            if( data && !data.currentTarget ) self.options.data = data;
+            $.ajax( self.options ).success( self.success ).always( self.always ).error( self.error );
+        };
+    };
 
     commands.register = function( key, options ) {
-        if( !( this instanceof commands.register )) {
-            return new commands.register( key, options );
-        }
+        checkArg.required( key, 'Please provide a key to register' );
+        checkArg.required( options, 'You must provide an options object with a URL property' );
         
-        var self = this;
-        $.extend( options, _defaultOptions );
-        
-        commands.execute[key] = function( data ) {
-            if( data && !data.currentTarget ) options.data = data;
-            $.ajax( options ).success( self.success ).always( self.always ).error( self.error );
-        };
-
-        return this;
+        var command = new Command( key, $.extend( options, _defaultOptions ) );
+        commands[key] = command;
+        return command;
     };
 
-    commands.register.fn = commands.register.prototype;
+    Command.fn = Command.prototype;
 
-    commands.register.fn.setCallback = function( func, pointer ) {
-        this[pointer] = func;
+    Command.fn.bind = function( selector, events, func ) {
+        var self = this,
+            data;
+        data = ( _isFunction( func ) ? func() : data;
+        $( window ).on( events, selector, function( evt ) {
+            evt.preventDefault();
+            self.send( data );
+            return this;
+        });
     };
         
-    commands.register.fn.success = function( func ) {
+    Command.fn.success = function( func ) {
         if( func && _isFunction( func ) ) {
-            this.setCallback( func, 'success' );
-        }
-        return this;
-    };
-    
-    commands.register.fn.always = function( func ) {
-        if( func && _isFunction( func ) ) {
-            this.setCallback( func, 'always' );
+            this.success = $.proxy(func, this);
         }
         return this;
     };
     
-    commands.register.fn.error = function( func ) {
+    Command.fn.always = function( func ) {
         if( func && _isFunction( func ) ) {
-            this.setCallback( func, 'error' );
+            this.always = func;
+        }
+        return this;
+    };
+    
+    Command.fn.error = function( func ) {
+        if( func && _isFunction( func ) ) {
+            this.error = func;
         }
         return this;
     };
@@ -53,8 +71,7 @@ var commands = ( function( $ ) {
     _isFunction = function( obj ) {
         return Object.prototype.toString.call( obj ) === "[object Function]";
     }
-    
-    commands.send = commands.execute;
+
     return commands;
 
 })( jQuery );
